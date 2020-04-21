518c7bba0a01c5f4d633e40d25d5dafbaca35c8c
Vitalii Potapov <vitalii.potapov92@gmail.com>
2020-04-21 07:13:58 +0600

    add usability

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function AES_Init() {
   AES_Sbox_Inv = new Array(256);
-  for(var i = 0; i < 256; i++)
+  for (var i = 0; i < 256; i++)
     AES_Sbox_Inv[AES_Sbox[i]] = i;
 
   AES_ShiftRowTab_Inv = new Array(16);
-  for(var i = 0; i < 16; i++)
+  for (var i = 0; i < 16; i++)
     AES_ShiftRowTab_Inv[AES_ShiftRowTab[i]] = i;
 
   AES_xtime = new Array(256);
-  for(var i = 0; i < 128; i++) {
+  for (var i = 0; i < 128; i++) {
     AES_xtime[i] = i << 1;
     AES_xtime[128 + i] = (i << 1) ^ 0x1b;
   }
@@ -29,15 +29,15 @@ function AES_ExpandKey(key) {
     default:
       alert("AES_ExpandKey: Only key lengths of 16, 24 or 32 bytes allowed!");
   }
-  for(var i = kl; i < ks; i += 4) {
+  for (var i = kl; i < ks; i += 4) {
     var temp = key.slice(i - 4, i);
     if (i % kl == 0) {
       temp = new Array(AES_Sbox[temp[1]] ^ Rcon, AES_Sbox[temp[2]], AES_Sbox[temp[3]], AES_Sbox[temp[0]]);
-      if ((Rcon <<= 1) >= 256) {Rcon ^= 0x11b;}
+      if ((Rcon <<= 1) >= 256) { Rcon ^= 0x11b; }
     } else if ((kl > 24) && (i % kl == 16)) {
       temp = new Array(AES_Sbox[temp[0]], AES_Sbox[temp[1]], AES_Sbox[temp[2]], AES_Sbox[temp[3]]);
     }
-    for(var j = 0; j < 4; j++) {
+    for (var j = 0; j < 4; j++) {
       key[i + j] = key[i + j - kl] ^ temp[j];
     }
   }
@@ -46,7 +46,7 @@ function AES_ExpandKey(key) {
 function AES_Encrypt(block, key) {
   var l = key.length;
   AES_AddRoundKey(block, key.slice(0, 16));
-  for(var i = 16; i < l - 16; i += 16) {
+  for (var i = 16; i < l - 16; i += 16) {
     AES_SubBytes(block, AES_Sbox);
     AES_ShiftRows(block, AES_ShiftRowTab);
     AES_MixColumns(block);
@@ -62,7 +62,7 @@ function AES_Decrypt(block, key) {
   AES_AddRoundKey(block, key.slice(l - 16, l));
   AES_ShiftRows(block, AES_ShiftRowTab_Inv);
   AES_SubBytes(block, AES_Sbox_Inv);
-  for(var i = l - 32; i >= 16; i -= 16) {
+  for (var i = l - 32; i >= 16; i -= 16) {
     AES_AddRoundKey(block, key.slice(i, i + 16));
     AES_MixColumns_Inv(block);
     AES_ShiftRows(block, AES_ShiftRowTab_Inv);
@@ -71,41 +71,41 @@ function AES_Decrypt(block, key) {
   AES_AddRoundKey(block, key.slice(0, 16));
 }
 
-AES_Sbox = new Array(99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,
-  118,202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,183,253,
-  147,38,54,63,247,204,52,165,229,241,113,216,49,21,4,199,35,195,24,150,5,154,
-  7,18,128,226,235,39,178,117,9,131,44,26,27,110,90,160,82,59,214,179,41,227,
-  47,132,83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,208,239,170,
-  251,67,77,51,133,69,249,2,127,80,60,159,168,81,163,64,143,146,157,56,245,
-  188,182,218,33,16,255,243,210,205,12,19,236,95,151,68,23,196,167,126,61,
-  100,93,25,115,96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,224,
-  50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,231,200,55,109,141,213,
-  78,169,108,86,244,234,101,122,174,8,186,120,37,46,28,166,180,198,232,221,
-  116,31,75,189,139,138,112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,
-  158,225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,140,161,
-  137,13,191,230,66,104,65,153,45,15,176,84,187,22);
+AES_Sbox = new Array(99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171,
+  118, 202, 130, 201, 125, 250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192, 183, 253,
+  147, 38, 54, 63, 247, 204, 52, 165, 229, 241, 113, 216, 49, 21, 4, 199, 35, 195, 24, 150, 5, 154,
+  7, 18, 128, 226, 235, 39, 178, 117, 9, 131, 44, 26, 27, 110, 90, 160, 82, 59, 214, 179, 41, 227,
+  47, 132, 83, 209, 0, 237, 32, 252, 177, 91, 106, 203, 190, 57, 74, 76, 88, 207, 208, 239, 170,
+  251, 67, 77, 51, 133, 69, 249, 2, 127, 80, 60, 159, 168, 81, 163, 64, 143, 146, 157, 56, 245,
+  188, 182, 218, 33, 16, 255, 243, 210, 205, 12, 19, 236, 95, 151, 68, 23, 196, 167, 126, 61,
+  100, 93, 25, 115, 96, 129, 79, 220, 34, 42, 144, 136, 70, 238, 184, 20, 222, 94, 11, 219, 224,
+  50, 58, 10, 73, 6, 36, 92, 194, 211, 172, 98, 145, 149, 228, 121, 231, 200, 55, 109, 141, 213,
+  78, 169, 108, 86, 244, 234, 101, 122, 174, 8, 186, 120, 37, 46, 28, 166, 180, 198, 232, 221,
+  116, 31, 75, 189, 139, 138, 112, 62, 181, 102, 72, 3, 246, 14, 97, 53, 87, 185, 134, 193, 29,
+  158, 225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223, 140, 161,
+  137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22);
 
-AES_ShiftRowTab = new Array(0,5,10,15,4,9,14,3,8,13,2,7,12,1,6,11);
+AES_ShiftRowTab = new Array(0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12, 1, 6, 11);
 
 function AES_SubBytes(state, sbox) {
-  for(var i = 0; i < 16; i++) {
+  for (var i = 0; i < 16; i++) {
     state[i] = sbox[state[i]];
   }
 }
 
 function AES_AddRoundKey(state, rkey) {
-  for(var i = 0; i < 16; i++)
+  for (var i = 0; i < 16; i++)
     state[i] ^= rkey[i];
 }
 
 function AES_ShiftRows(state, shifttab) {
   var h = new Array().concat(state);
-  for(var i = 0; i < 16; i++)
+  for (var i = 0; i < 16; i++)
     state[i] = h[shifttab[i]];
 }
 
 function AES_MixColumns(state) {
-  for(var i = 0; i < 16; i += 4) {
+  for (var i = 0; i < 16; i += 4) {
     var s0 = state[i + 0], s1 = state[i + 1];
     var s2 = state[i + 2], s3 = state[i + 3];
     var h = s0 ^ s1 ^ s2 ^ s3;
@@ -117,7 +117,7 @@ function AES_MixColumns(state) {
 }
 
 function AES_MixColumns_Inv(state) {
-  for(var i = 0; i < 16; i += 4) {
+  for (var i = 0; i < 16; i += 4) {
     var s0 = state[i + 0], s1 = state[i + 1];
     var s2 = state[i + 2], s3 = state[i + 3];
     var h = s0 ^ s1 ^ s2 ^ s3;
@@ -168,9 +168,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const keyField = document.querySelector('#key');
   const result = document.querySelector('#result');
   const check = document.querySelector('#check');
+  const showResult = document.querySelectorAll('.show-result');
   document.addEventListener('keydown', (el) => {
-    result.innerText = '';
-    check.innerText = '';
+    if (!el.ctrlKey && !el.shiftKey && !el.altKey) {
+      result.innerText = '';
+      check.innerText = '';
+    }
     if (el.keyCode === 13) {
       let text;
       let key = keyField.value;
@@ -187,5 +190,26 @@ document.addEventListener('DOMContentLoaded', () => {
         result.innerText = decryptAES(text, key);
       };
     }
+  });
+  result.addEventListener('click', (el) => {
+    const fieldCopyText = document.createElement('input');
+    fieldCopyText.setAttribute('value', result.innerText);
+    fieldCopyText.style.opacity = 0;
+    document.querySelector('body').appendChild(fieldCopyText);
+    fieldCopyText.select();
+    document.execCommand('copy');
+    fieldCopyText.remove();
+    showResult[0].classList.remove('hide');
+    result.innerText = 'Result saved to clipboard';
+    setTimeout(() => {
+      result.innerText = '';
+      showResult[0].classList.add('hide');
+    }, 3000);
+  })
+  showResult.forEach((el) => {
+    el.addEventListener('click', ({ target }) => {
+      target.classList.remove('hide');
+      setTimeout(() => { target.classList.add('hide'); }, 500);
+    });
   });
 });
